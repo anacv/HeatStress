dafa34a688daa700473187b57944c4a1f4db623f
Jonas Bhend <jonas.bhend@meteoswiss.ch>
2017-02-21 15:48:52 +0100

    Performance enhancement to wbgt.Bernard, roxygen version

--- R/wbgt.Bernard.R
+++ R/wbgt.Bernard.R
@@ -4,7 +4,8 @@
 #' 
 #' @param tas: vector of air temperature in degC.
 #' @param dewp: vector of dew point temperature in degC.
-#' @param tolerance (optional): tolerance value for the iteration. Default: 1e-6.
+#' @param tolerance (optional): tolerance value for the iteration. Default: 1e-4.
+#' @param noNAs: logical, should \code{tas >= dewp} be enforced by swapping?
 #' 
 #' @return A list of:
 #' @return $value: wet bulb globe temperature in degC.
@@ -21,7 +22,7 @@
 #' 
 
 
-wbgt.Bernard <- function(tas, dewp, tolerance= 1e-6){
+wbgt.Bernard <- function(tas, dewp, tolerance= 1e-4, noNAs=FALSE){
 
 
 ##################################################
@@ -35,54 +36,56 @@ c4 <- 1556
 c5 <- 1.484
 c6 <- 1010
 
+# pre-allocate output
 ndates <- length(tas)
+Tpwb <- rep(NA, ndates)
 
 
 # **************************************************************************************
 # *** calculate the phychrometric wet bulb temperature (Tpwb) in degC, by iteration ***
 # **************************************************************************************
-Tpwb <- rep(NA, ndates)
-data <- rep(NA, ndates)
-wbgt <- list()
-
 # Function to minimize
 fr <- function(TT,tasi,edi) {  
   abs(c4*edi - c5*edi*TT - c4*c1*exp((c2*TT)/(c3+TT)) + c5*c1*exp((c2*TT)/(c3+TT))*TT + c6*(tasi-TT))
 }
 
 # Filter the case when tas=dewp (RH=100)
-trivial <- abs(tas - dewp) < 1e-4 
+trivial <- abs(tas - dewp) < tolerance
 Tpwb[which(trivial)] <- tas[which(trivial)]
-data[which(trivial)] <- 0.67*Tpwb[which(trivial)] + 0.33*tas[which(trivial)]
 
 # Filter data to calculate the WBGT with optimization function
 xmask <- !is.na(tas + dewp) & !trivial 
 
+## swap temperature and dewpoint if necessary
+if (noNAs){
+  tastmp <- pmax(tas, dewp)
+  dewp <- pmin(tas, dewp)
+  tas <- tastmp
+} else {
+  xmask <- xmask & tas >= dewp
+}
+
+
+# ********************************************************************
+# *** calculate the vapour pressure from the dew point (ed) in hPa ***
+# ********************************************************************
+ed <- c1 * exp((c2*dewp)/(c3+dewp))
+
 for (i in which(xmask)){
-  # if dewp>tas, use dewp for tas and viceversa
-  tas1 <- max(tas[i], dewp[i])
-  dewp1 <- min(tas[i], dewp[i])
-  
-  # ********************************************************************
-  # *** calculate the vapour pressure from the dew point (ed) in hPa ***
-  # ********************************************************************
-  ed1 <- c1 * exp((c2*dewp1)/(c3+dewp1))
-  
+
   # ********************
   # *** minimization ***
   # ********************
-  opt <- optimize(fr, range(tas1+1, dewp1-1),tasi=tas1, edi=ed1, tol=tolerance)
+  opt <- optimize(fr, range(tas[i]+1, dewp[i]-1),tasi=tas[i], edi=ed[i], tol=tolerance)
   Tpwb[i]=opt$minimum
-  
-  # *******************************
-  # *** Calculation of the WBGT ***
-  # *******************************
-  data[i] <- 0.67*Tpwb[i] + 0.33*tas1
   
 }
 
-wbgt$value <- data
-wbgt$tpwb <- Tpwb
+# *******************************
+# *** Calculation of the WBGT ***
+# *******************************
+wbgt <- list(Tpwb = Tpwb, 
+             data = 0.67*Tpwb + 0.33 * tas)
 
 return(wbgt)
 }
